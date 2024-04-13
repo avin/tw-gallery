7821a308c7167e57b5bcfece274027f8a001e0ea
a.vinogradov <a.vinogradov@bpcbt.com>
2024-04-13 14:24:27 +0300

    upd

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,14 +5,21 @@ import FilesTree from '@/components/FilesTree/FilesTree.tsx';
 import HtmlComponent from '@/components/HtmlComponent/HtmlComponent.tsx';
 
 const App = () => {
-  const [activeItem, activeActiveItem] = useState<string[]>(window.location.hash.replace(/^#/, '').split('/'));
+  const [activeItem, activeActiveItem] = useState<string[]>(
+    window.location.hash.replace(/^#/, '').split('/'),
+  );
 
   const category = useMemo(() => {
     const f = get(directoryTree, activeItem);
     return f;
   }, [activeItem]);
 
-  const originalSiteLink = `https://tailwindui.com/components/${activeItem.join('/')}`;
+  const originalSiteLink = useMemo(() => {
+    if (!activeItem.length) {
+      return null;
+    }
+    return `https://tailwindui.com/components/${activeItem.join('/')}`;
+  }, [activeItem]);
 
   useEffect(() => {
     window.location.hash = activeItem.join('/');
@@ -34,16 +41,19 @@ const App = () => {
 
       <div>
         <div className="p-4 overflow-y-auto max-h-[100vh]">
-          <div className="mb-4">
-            <a
-              href={originalSiteLink}
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Original gallery
-            </a>
-          </div>
+          {originalSiteLink && (
+            <div className="mb-4">
+              <a
+                href={originalSiteLink}
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Original gallery
+              </a>
+            </div>
+          )}
+
           {category &&
             Object.keys(category).map((key) => {
               return (
